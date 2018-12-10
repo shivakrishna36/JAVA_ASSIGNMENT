6dcf034e27b5807df80f021ebd28f59e18ceba22
GUNJI <shivakrishna.gunji@capgemini.com>
2018-12-10 12:46:15 +0530

    Adds java files

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+class Calculator 	//creating a class
+{
+	//creating  a main
+	public static void main(String args[])
+	{		
+		System.out.println("Hello World");
+		System.out.println("Addition is:" + add(20.4,10.2));
+		System.out.println("Subtraction is:" + sub(20.8,10.2));
+		System.out.println("Multiplication is:" + mul(20,10.4));
+		System.out.println("Division is:" + div(20,12));
+		
+	}
+	static double add(double a,double b)
+	{
+		double addition = one + two;
+		return addition;
+	}
+	static double sub(double one, double two)
+	{
+		double subtraction = one-two;
+		return c;
+	}
+	static double mul(double one,double two)
+	{
+		double multiplication = one*two;
+		return multiplication;
+	}
+	static double div(double one,double two)
+	{
+		double division = one/two;
+		return division;
+	}	
+
+}
---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello 	//creating a class
+{
+	//creating  a main
+	public static void main(String args[])
+	{		
+		display();
+		System.out.println("Hello world:");
+		display();
+	}
+	static void display()
+	{
+		System.out.println("This:");
+	}
+}
